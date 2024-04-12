13221e2fd4d1abac395ead4672d9ad4cda0debe7
khaledshuvo <khaledsbd@hotmail.com>
2024-04-12 15:56:25 +0600

    .env restored to check github whether .env name in .gitignore works

--- src/Pages/UpdateProfile/UpdateProfile.jsx
+++ src/Pages/UpdateProfile/UpdateProfile.jsx
@@ -20,14 +20,16 @@ const UpdateProfile = () => {
   };
 
   return (
-    <div className='mb-10'>
+    <div className="mb-10">
       <Helmet>
         <title>Lux Houzez | Update Profile</title>
       </Helmet>
       <h2 className="text-2xl my-10 text-center">Update your Profile</h2>
       <form onSubmit={handleRegister} className=" md:w-3/4 lg:w-1/2 mx-auto">
         <div className="form-control">
-          <label className="label label-text">Name</label>
+          <label className="label label-text text-base font-semibold">
+            Your Name
+          </label>
           <input
             type="text"
             required
@@ -38,7 +40,9 @@ const UpdateProfile = () => {
           />
         </div>
         <div className="form-control">
-          <label className="label label-text">Photo URL</label>
+          <label className="label label-text text-base font-semibold mt-5">
+            Your Photo URL
+          </label>
           <input
             type="text"
             required
@@ -49,7 +53,9 @@ const UpdateProfile = () => {
           />
         </div>
         <div className="form-control">
-          <label className="label label-text">Email</label>
+          <label className="label label-text text-red-500 text-base font-semibold mt-5">
+            Your Email {'(unchangeable)'}{' '}
+          </label>
           <input
             type="email"
             required
